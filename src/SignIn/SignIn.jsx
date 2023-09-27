d90bce4cfeb334e5d2986fc06800d0693a864679
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-28 02:07:40 +0700

    nyoba animation

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react'
 import { useSpring, useSpringRef, animated } from '@react-spring/web'
+import "animate.css"
 
 export default function SignIn(){
   const api = useSpringRef()
-  const springs = useSpring({
+  const jumbo = useSpring({
     ref: api,
     from: { x: 0 },
   })
   
   const handleClick = () => {
-    console.log(screen.width)
+    if(jumbo.x.get() === window.innerWidth/2){
+      //Animate kiri ngilang
+      document.getElementById("left").classList.add('animate__animated', 'animate__fadeOutRight')
+      
+      //Animate kanan muncul
+      document.getElementById("left").classList.add('animate__animated', 'animate__fadeInLeft')
+    }else{
+      //Animate kiri nmuncul
+      document.getElementById("left").classList.add('animate__animated', 'animate__fadeInRight')
+      
+      //Animate kanan ngilang
+      document.getElementById("left").classList.add('animate__animated', 'animate__fadeOutLeft')
+    }
     api.start({
       to: {
-        x: springs.x.get() === screen.width/2 ? 0 : screen.width/2,
+        x: jumbo.x.get() === window.innerWidth/2 ? 0 : window.innerWidth/2,
       },
     })
   }
 
   return (
     <>
-      <div className="bg-slate-600 font-bold h-screen">
+      <div className="bg-ghostwhite font-bold h-screen flex">
         <animated.div
           onClick={handleClick}
           style={{
             width: "50%",
             height: "100%",
-            background: '#ff6d6d',
-            ...springs,
+            zIndex: 2,
+            ...jumbo,
           }}
-        />
+          className=" bg-navyblue fixed"
+        >
+          
+        </animated.div>
+        <div className='left w-1/2 h-screen'>
+          <div id='left'>
+            asdsa
+          </div>
+        </div>
+        <div className='right w-1/2 h-screen'>
+          xcvzx
+        </div>
       </div>
     
     </>
