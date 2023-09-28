f655335a0456206a13c54735c7fc3f25d4b69a1c
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-28 19:21:26 +0700

    sign in blm selesai
    jangan lupa di npm i

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useSpring, useSpringRef, animated } from '@react-spring/web'
 import "animate.css"
+import { Input, InputGroup, InputRightElement, Button, Checkbox } from '@chakra-ui/react'
 
 export default function SignIn(){
   const api = useSpringRef()
@@ -8,26 +9,63 @@ export default function SignIn(){
     ref: api,
     from: { x: 0 },
   })
+
+  let move = false
+
+  const [showPass, setShowPass] = useState(false)
+  const handlePass = () => setShowPass(!showPass)
+  
+  const [showConfirm, setShowConfirm] = useState(false)
+  const handleConfirm = () => setShowConfirm(!showConfirm)
   
   const handleClick = () => {
-    if(jumbo.x.get() === window.innerWidth/2){
-      //Animate kiri ngilang
-      document.getElementById("left").classList.add('animate__animated', 'animate__fadeOutRight')
-      
-      //Animate kanan muncul
-      document.getElementById("left").classList.add('animate__animated', 'animate__fadeInLeft')
-    }else{
-      //Animate kiri nmuncul
-      document.getElementById("left").classList.add('animate__animated', 'animate__fadeInRight')
-      
-      //Animate kanan ngilang
-      document.getElementById("left").classList.add('animate__animated', 'animate__fadeOutLeft')
+    
+    if(move == false){
+      move = true
+      if(jumbo.x.get() === window.innerWidth/2){
+        // Tambah class Animate kiri ngilang
+        document.getElementById("left").classList.add('animate__animated', 'animate__fadeOutRight')
+        
+        // Tambah class Animate kanan muncul
+        document.getElementById("right").classList.add('animate__animated', 'animate__fadeInLeft')
+      }else{
+        // Tambah class Animate kiri nmuncul
+        document.getElementById("left").classList.add('animate__animated', 'animate__fadeInRight')
+        
+        // Tambah class Animate kanan ngilang
+        document.getElementById("right").classList.add('animate__animated', 'animate__fadeOutLeft')
+      }
+  
+      api.start({
+        to: {
+          x: jumbo.x.get() === window.innerWidth/2 ? 0 : window.innerWidth/2,
+        },
+      })
+
+      move = true
     }
-    api.start({
-      to: {
-        x: jumbo.x.get() === window.innerWidth/2 ? 0 : window.innerWidth/2,
-      },
-    })
+    
+    if(move == true){
+      setTimeout(() => {
+        console.log(jumbo.x.get())
+        if(jumbo.x.get() === window.innerWidth/2){
+          // Hapus class Animate kiri ngilang
+          document.getElementById("left").classList.remove('animate__animated', 'animate__fadeInRight')
+          
+          // Hapus class Animate kanan muncul
+          document.getElementById("right").classList.remove('animate__animated', 'animate__fadeOutLeft')
+        }else{
+          // Hapus class Animate kiri nmuncul
+          document.getElementById("left").classList.remove('animate__animated', 'animate__fadeOutRight')
+          
+          // Hapus class Animate kanan ngilang
+          document.getElementById("right").classList.remove('animate__animated', 'animate__fadeInLeft')
+        }
+        move = false
+      }, 1000)
+    }
+    
+
   }
 
   return (
@@ -41,18 +79,102 @@ export default function SignIn(){
             zIndex: 2,
             ...jumbo,
           }}
-          className=" bg-navyblue fixed"
+          className=" bg-navyblue-800 fixed"
         >
           
         </animated.div>
-        <div className='left w-1/2 h-screen'>
-          <div id='left'>
-            asdsa
+        <div className='left w-1/2 h-screen flex justify-center items-center text-navyblue-800'>
+          <div id='left' className=' w-5/6 h-[33rem] border p-10'>
+            <h1 className=' text-5xl text-center mb-10'>Sign Up</h1>
+
+            <p>Username</p>
+            <Input variant='outline' placeholder='Username' />
+            <p>Email</p>
+            <Input variant='outline' placeholder='Email' />
+            
+            <p>Password</p>
+            <InputGroup>
+              <Input
+                pr='4.5rem'
+                type={showPass ? 'text' : 'password'}
+                placeholder='Enter password'
+              />
+              <InputRightElement width='4.5rem'>
+                <Button h='1.75rem' size='sm' onClick={handlePass}>
+                  {showPass ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
+            <div className='flex w-full justify-center'>
+              <div className='w-4/6'>
+                <Button className='w-full bg-navyblue'>Sign In</Button>
+
+              </div>
+
+            </div>
           </div>
         </div>
-        <div className='right w-1/2 h-screen'>
-          xcvzx
+        <div className='right w-1/2 h-screen flex justify-center items-center text-navyblue-800'>
+          <div id='right' className=' w-5/6 h-[33rem] border p-10'>
+            <h1 className=' text-5xl text-center mb-10'>Sign In</h1>
+
+            <p>Username</p>
+            <Input variant='outline' placeholder='Username' />
+            <p>Email</p>
+            <Input variant='outline' placeholder='Email' />
+            
+            <div className='flex justify-between'>
+              <div className=' w-[49%]'>
+                <p>Password</p>
+                <InputGroup>
+                  <Input
+                    pr='4.5rem'
+                    type={showPass ? 'text' : 'password'}
+                    placeholder='Enter password'
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handlePass}>
+                      {showPass ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </div>
+
+              <div className=' w-[49%]'>
+                <p>Confirm Password</p>
+                <InputGroup>
+                  <Input
+                    pr='4.5rem'
+                    type={showConfirm ? 'text' : 'password'}
+                    placeholder='Enter password'
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleConfirm}>
+                      {showConfirm ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </div>
+
+            </div>
+            <div className='flex w-full justify-center'>
+              <div className='w-4/6'>
+                <Checkbox >I agree to the Terms and Conditions.*</Checkbox>
+
+              </div>
+
+            </div>
+            <div className='flex w-full justify-center'>
+              <div className='w-4/6'>
+                <Button colorScheme={'navyblue'} color={'ghostwhite'} className='w-full'>Sign In</Button>
+
+              </div>
+
+            </div>
+          </div>
         </div>
+
       </div>
     
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,83 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    ghostwhite: 
+    {
+      50: "#f8f8ff",
+      100: "#dfdfe5",
+      200: "#c6c6cc",
+      300: "#adadb2",
+      400: "#949499",
+      500: "#7c7c7f",
+      600: "#636366",
+      700: "#4a4a4c",
+      800: "#313133",
+      900: "#181819",
+    },
+    lightblue: 
+    {
+      50: '#eaf3fc',
+      100: '#d2d8e0',
+      200: '#b8bec6',
+      300: '#9da4ae',
+      400: '#828a96',
+      500: '#69717d',
+      600: '#515862',
+      700: '#393f47',
+      800: '#21262d',
+      900: '#030e16',
+    },
+    indigo: 
+    {
+      50: '#e7e7ff',
+      100: '#b8bbfd',
+      200: '#8a8ef6',
+      300: '#5b60f0',
+      400: '#2e33eb',
+      500: '#161ad2',
+      600: '#0f14a4',
+      700: '#080e76',
+      800: '#030749',
+      900: '#00021d',
+    },
+    navyblue: 
+    {
+      50: '#edeff7',
+      100: '#cbd0e3',
+      200: '#a8b0d1',
+      300: '#8691c1',
+      400: '#6371b2',
+      500: '#4a5898',
+      600: '#3a4475',
+      700: '#2a3154',
+      800: '#191d32',
+      900: '#080a12',
+    },
+    darkblue: 
+    {
+      50: '#ebf1fc',
+      100: '#ccd4e5',
+      200: '#acb7d1',
+      300: '#8b9abf',
+      400: '#6a7dad',
+      500: '#506493',
+      600: '#3e4d73',
+      700: '#2c3752',
+      800: '#1a2133',
+      900: '#070b15',
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={Router} />
+
+    </ChakraProvider>
   </React.StrictMode>,
 )
