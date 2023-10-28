d5c6709e033228e994ae8b13edd9f60f0b3b859a
Jonathan Wilbert <jonathanwilbert8wd@gmail.com>
2023-10-28 22:15:33 +0700

    Add Padding menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,20 +9,20 @@ export default function Menu() {
   return (
     <>
       <div className="h-[calc(100vh-5rem)] w-1/4 sticky top-[5rem]">
-        <div className="flex  flex-col gap-10 p-10">
-          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start">
+        <div className="flex  flex-col">
+          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start p-10">
             <FaHouseChimney className="mr-2" size={25} />
             Home
           </button>
-          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start">
+          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start p-10">
             <FaEnvelope className="mr-2" size={25} />
             Messages
           </button>
-          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start">
+          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start p-10">
             <FaListUl className="mr-2" size={25} />
             List
           </button>
-          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start">
+          <button className="w-full h-10 hover:bg-ghostwhite-100 top-0 transition-colors duration-300 text-4xl flex items-center justify-start p-10">
             <FaPenToSquare className="mr-2" size={25} />
             Post
           </button>
