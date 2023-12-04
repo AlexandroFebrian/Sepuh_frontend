2177e9a30f5803d6b4a730007f713cde7287da5f
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-12-04 12:31:36 +0700

    Update Messages.jsx

--- src/Views/User/Messages/Messages.jsx
+++ src/Views/User/Messages/Messages.jsx
@@ -93,10 +93,24 @@ export default function Messages() {
                                             <div className="w-4/5">
                                                 <div className="w-full flex">
                                                     <p className="w-1/2 font-semibold text-xl">{ c.users.find((u) => u.user.email != user.email).user.name }</p>
-                                                    <p className="w-1/2 text-sm opacity-80 text-end">{ new Date(c.messages[c.messages.length - 1].time).getHours().toString().padStart(2, "0") + ":" + new Date(c.messages[c.messages.length - 1].time).getMinutes().toString().padStart(2, "0") }</p>
+                                                    {
+                                                        c.messages[c.messages.length - 1]
+                                                        &&
+                                                        <p className="w-1/2 text-sm opacity-80 text-end">
+                                                            {
+                                                                new Date(c.messages[c.messages.length - 1].time).getHours().toString().padStart(2, "0")
+                                                                + ":" +
+                                                                new Date(c.messages[c.messages.length - 1].time).getMinutes().toString().padStart(2, "0")
+                                                            }
+                                                        </p>
+                                                    }
                                                 </div>
                                                 <div className="w-full flex">
-                                                    <p className="truncate">{ c.messages[c.messages.length - 1].value }</p>
+                                                    {
+                                                        c.messages[c.messages.length - 1]
+                                                        &&    
+                                                        <p className="truncate">{ c.messages[c.messages.length - 1].value }</p>
+                                                    }
                                                 </div>
                                             </div>
                                         </div>
@@ -113,25 +127,52 @@ export default function Messages() {
                         <div className="w-9/12 h-full relative">
                             <div className="w-full h-24 px-5 py-4 border-b-2 border-navyblue-800 bg-ghostwhite-50 relative z-10">
                                 <div className="flex items-center">
-                                    <Avatar bg="ghostwhite.400" size={"lg"} src={selectedChat.users.find((u) => u.user.email != user.email).user.profile_picture}/>
+                                    <Avatar bg="ghostwhite.400" size={"lg"} src={
+                                        selectedChat.users.find((u) => u.user.email != user.email).user.profile_picture
+                                    }/>
                                     <div className="w-full">
-                                        <h1 className="font-semibold text-3xl ms-6 duration-300">{ selectedChat.users.find((u) => u.user.email != user.email).user.name }</h1>
-                                        {onlineUsers.find((ou) => ou.user_id == selectedChat.users.find((u) => u.user._id != user._id).user._id) && <p className="text-green-500 ms-6 duration-300">Online</p>}
+                                        <h1 className="font-semibold text-3xl ms-6 duration-300">
+                                            { selectedChat.users.find((u) => u.user.email != user.email).user.name }
+                                        </h1>
+                                        {
+                                            onlineUsers.find(
+                                                (ou) => ou.user_id == selectedChat.users.find(
+                                                    (u) => u.user._id != user._id
+                                                ).user._id
+                                            )
+                                            && 
+                                            <p className="text-green-500 ms-6 duration-300">Online</p>
+                                        }
                                     </div>
                                 </div>
                             </div>
                             <div className="absolute bottom-0 w-full h-[calc(100vh-11rem)] flex flex-col">
                                 <div ref={chatRef} className={"relative h-full w-full overflow-y-auto px-5 pb-5"}>
                                     { selectedChat.messages.map((m, i) => <div key={ i } className={"w-full flex flex-wrap items-end justify-" + (m.sender.email == user.email ? "end" : "start")}>
-                                        {m.sender.email == user.email && <p className="me-2">
-                                            { new Date(m.time).getHours().toString().padStart(2, "0") + ":" + new Date(m.time).getMinutes().toString().padStart(2, "0") }
-                                        </p>}
-                                        <div className={"border border-black p-5 max-w-[40%] mt-5 " + (m.sender.email == user.email ? "rounded-l-lg rounded-br-lg" : "rounded-r-lg rounded-bl-lg")}>
+                                        { m.sender.email == user.email && <p className="me-2">
+                                            {
+                                                new Date(m.time).getHours().toString().padStart(2, "0")
+                                                + ":" +
+                                                new Date(m.time).getMinutes().toString().padStart(2, "0")
+                                            }
+                                        </p> }
+                                        <div className={
+                                            "border border-black p-5 max-w-[40%] mt-5 " +
+                                                (
+                                                    m.sender.email == user.email ?
+                                                    "rounded-l-lg rounded-br-lg" : 
+                                                    "rounded-r-lg rounded-bl-lg"
+                                                )
+                                        }>
                                             { m.value }
                                         </div>
-                                        {m.sender.email != user.email && <p className="ms-2">
-                                            { new Date(m.time).getHours().toString().padStart(2, "0") + ":" + new Date(m.time).getMinutes().toString().padStart(2, "0") }
-                                        </p>}
+                                        { m.sender.email != user.email && <p className="ms-2">
+                                            {
+                                                new Date(m.time).getHours().toString().padStart(2, "0")
+                                                + ":" +
+                                                new Date(m.time).getMinutes().toString().padStart(2, "0")
+                                            }
+                                        </p> }
                                     </div>)}
                                 </div>
                                 <div className="w-full bg-white z-10 max-h-40 min-h-fit px-5 py-3 border-t-2 border-navyblue-800 transition-all duration-300">
@@ -160,7 +201,9 @@ export default function Messages() {
                                             <Button 
                                                 bg="ghostwhite.50"
                                                 height="3.1rem"
-                                                onClick={() => sendMessageHandler(selectedChat._id, selectedChat.users.find((u) => u.user.email != user.email).user._id)}
+                                                onClick={() => sendMessageHandler(
+                                                    selectedChat._id, selectedChat.users.find((u) => u.user.email != user.email).user._id
+                                                )}
                                             >
                                                 <FaRegPaperPlane />
                                             </Button>
