6633d89d64f8d4d14b5183c9a453ce09613f3d75
Jonathan Wilbert <jonathanwilbert8wd@gmail.com>
2023-12-12 10:09:00 +0700

    toggle between chart & table

--- src/Views/User/IncomeReports/IncomeReports.jsx
+++ src/Views/User/IncomeReports/IncomeReports.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 import { FaRegCalendarDays } from "react-icons/fa6";
@@ -14,6 +15,18 @@ import {
 } from "@/components/ui/popover";
 
 import Chart from "react-apexcharts";
+import IncomeReportsUserViewModel from "./IncomeReportsUserViewModel";
+import FreelancerProfileMenu from "../../../components/SidebarMenu/Freelancer/FreelancerProfileMenu/FreelancerProfileMenu";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from "@/components/ui/table";
 
 export default function IncomeReports() {
   const [selectedDays, setSelectedDays] = useState({
@@ -60,29 +73,41 @@ export default function IncomeReports() {
     },
   ]);
 
+  const [dataActive, setDataActive] = useState("Table");
+
+  const { isLogin, user } = IncomeReportsUserViewModel();
+
   return (
     <>
       <div className=" h-fit relative flex">
         <div className="left w-1/5 ">
           <div className="h-[calc(100vh-5rem)] w-full sticky top-[5rem]">
-            {/* {isLogin && user?.role == "Freelancer" && <FreelancerProfileMenu />} */}
+            {isLogin && user?.role == "Freelancer" && <FreelancerProfileMenu />}
             {/* {isLogin && user?.role == "Company" && <CompanyProfileMenu />} */}
           </div>
         </div>
-        <div className="mid w-3/5">
+        <div className="mid w-full">
           <div className=" min-h-[calc(100vh-5rem)] h-fit border-l-2 border-navyblue-600 z-0 px-10 py-10">
             <div className="w-full rounded py-6 px-10">
               <h1 className="font-semibold text-xl">Income Reports</h1>
-
               <div className="buttonTableOrChart mt-2">
-                <button className="bg-navyblue-700 text-white px-6 py-2 rounded-full">
+                <button
+                  className="bg-navyblue-700 text-white px-6 py-2 rounded-full"
+                  onClick={() => {
+                    setDataActive("Table");
+                  }}
+                >
                   Table
                 </button>
-                <button className="bg-navyblue-700 text-white px-6 py-2 rounded-full ml-3">
+                <button
+                  className="bg-navyblue-700 text-white px-6 py-2 rounded-full ml-3"
+                  onClick={() => {
+                    setDataActive("Chart");
+                  }}
+                >
                   Chart
                 </button>
               </div>
-
               <div
                 className="dateFromTo mt-5"
                 onClick={() => {
@@ -128,55 +153,97 @@ export default function IncomeReports() {
                   </Popover>
                 </div>
               </div>
-
-              <div className="chart mt-5">
-                <Chart
-                  options={{
-                    chart: {
-                      id: "basic-bar",
-                    },
-                    xaxis: {
-                      categories: [
-                        "Januari",
-                        "Februari",
-                        "Maret",
-                        "April",
-                        "Mei",
-                        "Juni",
-                        "Juli",
-                        "Agustus",
-                        "September",
-                        "Oktober",
-                        "November",
-                        "Desember",
-                      ],
-                    },
-                  }}
-                  series={[
-                    {
-                      name: "Income",
-                      data: [
-                        9500000, 5500000, 12500000, 3500000, 7500000, 10500000,
-                        1500000, 11500000, 6500000, 8500000, 4500000, 2500000,
-                      ],
-                    },
-                    {
-                      name: "Outcome",
-                      data: [
-                        1500000, 2500000, 3500000, 4500000, 5500000, 6500000,
-                        7500000, 8500000, 9500000, 10500000, 11500000, 12500000,
-                      ],
-                    },
-                  ]}
-                  type="line"
-                  width="100%"
-                  height="100%"
-                />
-              </div>
+              {dataActive == "Chart" && (
+                <div className="chart mt-5">
+                  <Chart
+                    options={{
+                      chart: {
+                        id: "basic-bar",
+                      },
+                      xaxis: {
+                        categories: [
+                          "Januari",
+                          "Februari",
+                          "Maret",
+                          "April",
+                          "Mei",
+                          "Juni",
+                          "Juli",
+                          "Agustus",
+                          "September",
+                          "Oktober",
+                          "November",
+                          "Desember",
+                        ],
+                      },
+                    }}
+                    series={[
+                      {
+                        name: "Income",
+                        data: [
+                          9500000, 5500000, 12500000, 3500000, 7500000,
+                          10500000, 1500000, 11500000, 6500000, 8500000,
+                          4500000, 2500000,
+                        ],
+                      },
+                      {
+                        name: "Outcome",
+                        data: [
+                          1500000, 2500000, 3500000, 4500000, 5500000, 6500000,
+                          7500000, 8500000, 9500000, 10500000, 11500000,
+                          12500000,
+                        ],
+                      },
+                    ]}
+                    type="line"
+                    width="100%"
+                    height="100%"
+                  />
+                </div>
+              )}
+              {dataActive == "Table" && (
+                <div className="bg-ghostwhite-100 mt-5 rounded-md">
+                  <Table className="w-full bg-ghostwhite-100 rounded-md">
+                    <TableHeader className="border-b-2 border-navyblue-600 ">
+                      <TableRow>
+                        <TableHead className="w-[100px] text-2xl text-navyblue-800 font-bold text-center">
+                          No
+                        </TableHead>
+                        <TableHead className="text-2xl text-navyblue-800 font-bold text-center">
+                          Month
+                        </TableHead>
+                        <TableHead className="text-2xl text-navyblue-800 font-bold text-center">
+                          Income
+                        </TableHead>
+                        <TableHead className="text-2xl text-navyblue-800 font-bold text-center">
+                          Outcome
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {data.map((item, index) => (
+                        <TableRow key={index}>
+                          <TableCell className="font-medium text-lg text-center">
+                            {index + 1}
+                          </TableCell>
+                          <TableCell className="font-medium text-lg text-center">
+                            {Object.keys(item)}
+                          </TableCell>
+                          <TableCell className="font-medium text-lg text-center">
+                            {Object.values(item)}
+                          </TableCell>
+                          <TableCell className="font-medium text-lg text-center">
+                            0
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <div className="right w-1/5"></div>
       </div>
     </>
   );
