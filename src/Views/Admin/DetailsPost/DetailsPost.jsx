664b551af341ce73fa0b953c884c9eeea14c090c
Jonathan Wilbert <jonathanwilbert8wd@gmail.com>
2023-12-14 14:46:22 +0700

    design sebagian detailpost

--- src/Views/Admin/DetailsPost/DetailsPost.jsx
+++ src/Views/Admin/DetailsPost/DetailsPost.jsx
@@ -1,9 +1,11 @@
 // EDISI PUTUS ASA PAKE VIEW MODEL
 import { useSearchParams } from "react-router-dom";
-// import DetailsPostViewModel from "./DetailsPostViewModel";
+import DetailsPostViewModel from "./DetailsPostViewModel";
 import { useEffect, useState } from "react";
 import Axios from "axios";
 const baseURL = import.meta.env.VITE_BACKEND_URL;
+import NavigationAdmin from "../../../components/NavigationAdmin/NavigationAdmin";
+import { FaEye } from "react-icons/fa";
 
 import {
   Accordion,
@@ -19,6 +21,8 @@ export default function DetailsPost() {
 
   const [emailSearch, setEmailSearch] = useState(email);
   const [dataDetailsPost, setDataDetailsPost] = useState([]);
+
+  const { getProfile, profile } = DetailsPostViewModel();
 
   const getDetailsPost = async () => {
     // localhost:3000/api/posts/admin/:email
@@ -80,105 +84,161 @@ export default function DetailsPost() {
 
   useEffect(() => {
     getDetailsPost();
+
+    getProfile(emailSearch);
   }, [emailSearch]);
+
+  const formatAmount = (amount) => {
+    const amountString = amount.toString();
+    const amountLength = amountString.length;
+    let amountFormatted = "";
+    for (let i = 0; i < amountLength; i++) {
+      if ((amountLength - i) % 3 === 0 && i !== 0) {
+        amountFormatted += ".";
+      }
+      amountFormatted += amountString[i];
+    }
+    amountFormatted = "Rp. " + amountFormatted + ",00";
+    return amountFormatted;
+  };
 
   return (
     <>
-      <div className="container-details">
-        {dataDetailsPost.map((item, index) => {
-          return (
-            <Accordion allowToggle key={index}>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <AccordionIcon />
-                    <div className="title-details">{item.title}</div>
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  <div className="container-details">
-                    <div className="container-details-image">
-                      {item.image.map((item, index) => {
-                        return (
-                          <img
-                            className="image-details"
-                            src={item}
-                            alt="image-details"
-                            key={index}
-                          />
-                        );
-                      })}
-                    </div>
-                    <div className="container-details-description">
-                      <div className="description-details">
-                        {item.description}
-                      </div>
-                      <div className="description-details">
-                        {item.hashtag.map((item, index) => {
+      <div className="container-details flex">
+        <div className="sideBar w-1/5 fixed left-0">
+          <NavigationAdmin />
+        </div>
+        <div className="right w-4/5 pt-10 absolute right-0">
+          <div className="top flex px-5 pb-5">
+            <div className="backButton">
+              <button
+                className="backButton bg-yellow-500 p-2 rounded-lg"
+                onClick={() => {
+                  window.history.back();
+                }}
+              >
+                Back
+              </button>
+            </div>
+            <div className="boxUserData px-5">
+              <div className="userData">
+                <div className="contentUserData text-3xl font-semibold">
+                  {emailSearch} Details Post
+                </div>
+              </div>
+            </div>
+          </div>
+          {console.log("dataDetailsPost", dataDetailsPost)}
+          {dataDetailsPost.map((item, index) => {
+            return (
+              <Accordion allowToggle key={index}>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <AccordionIcon />
+                      <div className="title-details">{item.title}</div>
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <div className="container-details">
+                      <div className="container-details-image">
+                        {item.image.map((item, index) => {
                           return (
-                            <div className="hashtag-details" key={index}>
-                              <div className="hashtag-details-text">{item}</div>
-                            </div>
+                            <img
+                              className="image-details w-1/5"
+                              src={item}
+                              alt="image-details"
+                              key={index}
+                            />
                           );
                         })}
                       </div>
-                      <div className="description-details">
-                        <div className="hashtag-details">
-                          <div className="hashtag-details-text">
-                            {item.min_price}
-                          </div>
+                      <div className="topDetails flex justify-between">
+                        <div className="title-details font-semibold text-2xl">
+                          {item.title}
                         </div>
-                        <div className="hashtag-details">
-                          <div className="hashtag-details-text">
-                            {item.max_price}
+
+                        <div className="price">
+                          <div className="price-details flex gap-2 text-xl">
+                            <div className="price-details-text">
+                              {formatAmount(item.min_price)}
+                            </div>
+                            <div className="price-details-text">-</div>
+                            <div className="price-details-text">
+                              {formatAmount(item.max_price)}
+                            </div>
                           </div>
                         </div>
                       </div>
+                      <div className="container-details-description">
+                        <div className="description-details font-semibold px-5">
+                          {item.description.length <= 0 ? (
+                            <div className="description-details-text">
+                              No description
+                            </div>
+                          ) : (
+                            item.description
+                          )}
+                        </div>
+                        <div className="description-details">
+                          {item.hashtag.map((item, index) => {
+                            return (
+                              <div className="hashtag-details" key={index}>
+                                <div className="hashtag-details-text flex gap-3 px-5">
+                                  {item.split(" ").map((item, index) => {
+                                    return (
+                                      <div
+                                        className="hashtag"
+                                        key={index}
+                                      >{`#${item}`}</div>
+                                    );
+                                  })}
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                      <div className="bottom flex justify-between px-5 mt-5 items-center">
+                        <div className="visitors">
+                          <div className="visitors-details flex gap-2 text-xl items-center">
+                            <div className="visitors-details-text">
+                              <FaEye />
+                            </div>
+                            <div className="visitors-details-text">
+                              {item.visitor} Visitors
+                            </div>
+                          </div>
+                        </div>
+                        <div className="container-details-button flex justify-end mr-5">
+                          {item.status === -1 ? (
+                            <button
+                              className="button-details bg-yellow-500 p-2 rounded-lg"
+                              onClick={() => {
+                                unsuspendPost(item._id);
+                              }}
+                            >
+                              Unsuspend
+                            </button>
+                          ) : (
+                            <button
+                              className="button-details  bg-yellow-500 p-2"
+                              onClick={() => {
+                                suspendPost(item._id);
+                              }}
+                            >
+                              Suspend
+                            </button>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                    <div className="container-details-button">
-                      {/* <button
-                        className="button-details  bg-yellow-500 mx-5 p-2"
-                        onClick={() => {
-                          suspendPost(item.id);
-                        }}
-                      >
-                        Suspend
-                      </button>
-                      <button
-                        className="button-details bg-yellow-500 mx-5 p-2"
-                        onClick={() => {
-                          unsuspendPost(item.id);
-                        }}
-                      >
-                        Unsuspend
-                      </button> */}
-
-                      {item.status === -1 ? (
-                        <button
-                          className="button-details bg-yellow-500 mx-5 p-2"
-                          onClick={() => {
-                            unsuspendPost(item._id);
-                          }}
-                        >
-                          Unsuspend
-                        </button>
-                      ) : (
-                        <button
-                          className="button-details  bg-yellow-500 mx-5 p-2"
-                          onClick={() => {
-                            suspendPost(item._id);
-                          }}
-                        >
-                          Suspend
-                        </button>
-                      )}
-                    </div>
-                  </div>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          );
-        })}
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            );
+          })}
+        </div>
       </div>
     </>
   );
