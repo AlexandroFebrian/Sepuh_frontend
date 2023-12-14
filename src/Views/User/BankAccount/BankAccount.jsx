3af526f2ffbe4729ef62f65d1cda85740e93992b
Jonathan Wilbert <jonathanwilbert8wd@gmail.com>
2023-12-14 13:40:48 +0700

    user?bank_name bikin frik

--- src/Views/User/BankAccount/BankAccount.jsx
+++ src/Views/User/BankAccount/BankAccount.jsx
@@ -7,100 +7,103 @@ import { Combobox } from "../../../components/ui/Combobox";
 import { useState, useRef, useEffect } from "react";
 
 export default function BankAccount() {
-    const { isLogin, user, profileAccountNumber, setProfileAccountNumber, bankName, saveProfile } = BankAccountViewModel();
-    // console.log(user);
-    const [bank_name, setBank_name] = useState(user?.bank_name);
-    // useEffect(() => {
-    //     console.log(user?.accountNumber);
-    // }, [user])
-    // const accountNumber = useRef();
-    return (
-        <>
-            <div className=" h-fit relative flex">
-                <div className="left w-1/5 ">
-                    <div className="h-[calc(100vh-5rem)] w-full sticky top-[5rem]">
-                        {isLogin && user?.role == "Freelancer" && <FreelancerProfileMenu />}
-                        {isLogin && user?.role == "Company" && <CompanyProfileMenu />}
-                    </div>
-                </div>
-                <div className="mid w-3/5">
-                    <div className=" min-h-[calc(100vh-5rem)] h-fit border-l-2 border-navyblue-600 z-0 px-10 py-10">
-                        <div className="w-full py-6 px-10 bg-ghostwhite-100 rounded-md">
-                            <h1 className="font-semibold text-xl">Bank Account</h1>
+  const {
+    isLogin,
+    user,
+    profileAccountNumber,
+    setProfileAccountNumber,
+    bankName,
+    saveProfile,
+  } = BankAccountViewModel();
+  const [bank_name, setBank_name] = useState(user?.bank_name);
+  return (
+    <>
+      <div className=" h-fit relative flex">
+        <div className="left w-1/5 ">
+          <div className="h-[calc(100vh-5rem)] w-full sticky top-[5rem]">
+            {isLogin && user?.role == "Freelancer" && <FreelancerProfileMenu />}
+            {isLogin && user?.role == "Company" && <CompanyProfileMenu />}
+          </div>
+        </div>
+        <div className="mid w-3/5">
+          <div className=" min-h-[calc(100vh-5rem)] h-fit border-l-2 border-navyblue-600 z-0 px-10 py-10">
+            <div className="w-full py-6 px-10 bg-ghostwhite-100 rounded-md">
+              <h1 className="font-semibold text-xl">Bank Account</h1>
 
-                            <div className="warningMessage">
-                                <Alert className="w-full bg-yellow-500 py-3 px-3 mt-5 rounded-md flex items-center gap-3">
-                                    <div className="left">
-                                        <FaCircleExclamation className="text-3xl" />
-                                    </div>
-                                    <div className="right">
-                                        <AlertTitle>
-                                            Please ensure that the bank account you input is correct.
-                                        </AlertTitle>
-                                        <AlertDescription>
-                                            The bank account you input will be used to receive your
-                                            income.
-                                        </AlertDescription>
-                                    </div>
-                                </Alert>
-                            </div>
+              <div className="warningMessage">
+                <Alert className="w-full bg-yellow-500 py-3 px-3 mt-5 rounded-md flex items-center gap-3">
+                  <div className="left">
+                    <FaCircleExclamation className="text-3xl" />
+                  </div>
+                  <div className="right">
+                    <AlertTitle>
+                      Please ensure that the bank account you input is correct.
+                    </AlertTitle>
+                    <AlertDescription>
+                      The bank account you input will be used to receive your
+                      income.
+                    </AlertDescription>
+                  </div>
+                </Alert>
+              </div>
 
-                            <div className="w-full mt-5 flex flex-col gap-1">
-                                <label
-                                    htmlFor="bankName"
-                                    className="font-semibold text-lg text-navyblue-800"
-                                >
-                                    Bank Name <span className="text-red-500">*</span>
-                                </label>
-                                <Combobox
-                                    title={"Select Bank Name"}
-                                    placeholder={"Select Bank Name"}
-                                    empty={"Bank Name not found"}
-                                    headerClassName={"w-72 shadow-lg "}
-                                    contentClassName={" pt-1 border-0 "}
-                                    className="w-full rounded-md border-2 border-navyblue-600 py-2 px-3 mt-2"
-                                    items={bankName}
-                                    item={user?.bank_name}
-                                    onSelect={(value) => {
-                                        setBank_name(value);
-                                        console.log(value);
-                                    }}
-                                />
-                            </div>
+              <div className="w-full mt-5 flex flex-col gap-1">
+                <label
+                  htmlFor="bankName"
+                  className="font-semibold text-lg text-navyblue-800"
+                >
+                  Bank Name <span className="text-red-500">*</span>
+                </label>
+                <Combobox
+                  title={"Select Bank Name"}
+                  placeholder={"Select Bank Name"}
+                  empty={"Bank Name not found"}
+                  headerClassName={"w-72 shadow-lg "}
+                  contentClassName={" pt-1 border-0 "}
+                  className="w-full rounded-md border-2 border-navyblue-600 py-2 px-3 mt-2"
+                  items={bankName}
+                  item={user?.bank_name}
+                  onSelect={(value) => {
+                    setBank_name(value);
+                  }}
+                />
+              </div>
 
-                            <div className="w-full mt-5 ">
-                                <label
-                                    htmlFor="accountNumber"
-                                    className="font-semibold text-lg text-navyblue-800"
-                                >
-                                    Account Number <span className="text-red-500">*</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    name="accountNumber"
-                                    id="accountNumber"
-                                    className="w-full rounded-md border-2 border-navyblue-600 py-2 px-3 mt-2"
-                                    placeholder="610xxxx"
-                                    onChange={(e) => {setProfileAccountNumber(e.target.value)}}
-                                    defaultValue={profileAccountNumber}
-                                />
-                            </div>
+              <div className="w-full mt-5 ">
+                <label
+                  htmlFor="accountNumber"
+                  className="font-semibold text-lg text-navyblue-800"
+                >
+                  Account Number <span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="accountNumber"
+                  id="accountNumber"
+                  className="w-full rounded-md border-2 border-navyblue-600 py-2 px-3 mt-2"
+                  placeholder="610xxxx"
+                  onChange={(e) => {
+                    setProfileAccountNumber(e.target.value);
+                  }}
+                  defaultValue={profileAccountNumber}
+                />
+              </div>
 
-                            <div className="w-full mt-5 flex justify-end">
-                                <button
-                                    className="w-28 py-1 bg-navyblue-700 text-white rounded-lg font-semibold text-xl border-2 border-navyblue-700 hover:bg-navyblue-800 hover:text-white duration-300 font-mono"
-                                    onClick={() => {
-                                        saveProfile(bank_name, profileAccountNumber);
-                                    }}
-                                >
-                                    Save
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="right w-1/5"></div>
+              <div className="w-full mt-5 flex justify-end">
+                <button
+                  className="w-28 py-1 bg-navyblue-700 text-white rounded-lg font-semibold text-xl border-2 border-navyblue-700 hover:bg-navyblue-800 hover:text-white duration-300 font-mono"
+                  onClick={() => {
+                    saveProfile(bank_name, profileAccountNumber);
+                  }}
+                >
+                  Save
+                </button>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="right w-1/5"></div>
+      </div>
+    </>
+  );
 }
--- src/Views/User/BankAccount/BankAccountViewModel.js
+++ src/Views/User/BankAccount/BankAccountViewModel.js
@@ -4,134 +4,128 @@ import { useSelector } from "react-redux";
 import fetch from "../../../Client/fetch";
 
 export default function BankAccountViewModel() {
-    const { checkToken, getUserProfile, getAllBankName, updateUserProfile } =
-        fetch();
+  const { checkToken, getUserProfile, getAllBankName, updateUserProfile } =
+    fetch();
 
-    const isLogin = useSelector((state) => state.user.isLogin);
-    const user = useSelector((state) => state.user.userDetail);
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const user = useSelector((state) => state.user.userDetail);
 
-    const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-    const [bankName, setBankName] = useState([]);
-    const [wait, setWait] = useState(false);
-    const [popup, setPopup] = useState(false);
-    const [popupTitle, setPopupTitle] = useState("");
-    const [popupButtonMessage, setPopupButtonMessage] = useState("");
-    const [popupType, setPopupType] = useState(false);
+  const [bankName, setBankName] = useState([]);
+  const [wait, setWait] = useState(false);
+  const [popup, setPopup] = useState(false);
+  const [popupTitle, setPopupTitle] = useState("");
+  const [popupButtonMessage, setPopupButtonMessage] = useState("");
+  const [popupType, setPopupType] = useState(false);
 
-    const [bannerFile, setBannerFile] = useState([]);
-    const [bannerImageSrc, setBannerImageSrc] = useState([]);
+  const [bannerFile, setBannerFile] = useState([]);
+  const [bannerImageSrc, setBannerImageSrc] = useState([]);
 
-    const [profileFile, setProfileFile] = useState([]);
-    const [profileImageSrc, setProfileImageSrc] = useState([]);
+  const [profileFile, setProfileFile] = useState([]);
+  const [profileImageSrc, setProfileImageSrc] = useState([]);
 
-    const [profileMemberSince, setProfileMemberSince] = useState("");
-    const [profileName, setProfileName] = useState("");
-    const [profileEmail, setProfileEmail] = useState("");
-    const [profileDateOfBirth, setProfileDateOfBirth] = useState("");
-    const [profileHeadline, setProfileHeadline] = useState("");
-    const [profileBio, setProfileBio] = useState("");
-    const [profileCity, setProfileCity] = useState("");
-    const [profileCountry, setProfileCountry] = useState("");
-    const [profileAccountNumber, setProfileAccountNumber] = useState("");
+  const [profileMemberSince, setProfileMemberSince] = useState("");
+  const [profileName, setProfileName] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
+  const [profileDateOfBirth, setProfileDateOfBirth] = useState("");
+  const [profileHeadline, setProfileHeadline] = useState("");
+  const [profileBio, setProfileBio] = useState("");
+  const [profileCity, setProfileCity] = useState("");
+  const [profileCountry, setProfileCountry] = useState("");
+  const [profileAccountNumber, setProfileAccountNumber] = useState("");
 
-    const [profileLastEducation, setProfileLastEducation] = useState("");
-    const [profileCurrentEducation, setProfileCurrentEducation] = useState("");
-    const [profileFieldOfStudy, setProfileFieldOfStudy] = useState("");
-    const [profileYearofStudy, setProfileYearofStudy] = useState("");
+  const [profileLastEducation, setProfileLastEducation] = useState("");
+  const [profileCurrentEducation, setProfileCurrentEducation] = useState("");
+  const [profileFieldOfStudy, setProfileFieldOfStudy] = useState("");
+  const [profileYearofStudy, setProfileYearofStudy] = useState("");
 
+  async function saveProfile(bank_name, account_number) {
+    if (isNaN(account_number)) {
+      alert("Account number must be a number");
+      return;
+    }
+    const data = {
+      header_picture: bannerFile.length == 0 ? undefined : bannerFile[0],
+      profile_picture: profileFile.length == 0 ? undefined : profileFile[0],
+      date_of_birth: profileDateOfBirth,
+      headline: profileHeadline,
+      bio: profileBio,
+      city: profileCity,
+      country: profileCountry,
+      last_education: profileLastEducation,
+      current_education: profileCurrentEducation,
+      field_of_study: profileFieldOfStudy,
+      year_of_study: profileYearofStudy,
+      bank_name: bank_name,
+      account_number: account_number,
+    };
 
+    setWait(true);
+    const response = await updateUserProfile(data, setWait, setPopup);
 
-    async function saveProfile(bank_name, account_number) {
-        if (isNaN(account_number)) {
-            alert("Account number must be a number");
-            return;
-        }
-
-        const data = {
-            header_picture: bannerFile.length == 0 ? undefined : bannerFile[0],
-            profile_picture: profileFile.length == 0 ? undefined : profileFile[0],
-            date_of_birth: profileDateOfBirth,
-            headline: profileHeadline,
-            bio: profileBio,
-            city: profileCity,
-            country: profileCountry,
-            last_education: profileLastEducation,
-            current_education: profileCurrentEducation,
-            field_of_study: profileFieldOfStudy,
-            year_of_study: profileYearofStudy,
-            bank_name: bank_name,
-            account_number: account_number,
-        };
-
-        setWait(true);
-        const response = await updateUserProfile(data, setWait, setPopup);
-
-        if (response == undefined) {
-            setPopupTitle("Network Error!");
-            setPopupButtonMessage("Try Again");
-            setPopupType(false);
-            return;
-        }
-
-        if (response.status.toString()[0] != 2) {
-            // console.log(response)
-            setPopupTitle(response.data.message);
-            setPopupButtonMessage("Try Again");
-            setPopupType(false);
-            return;
-        }
-
-        setPopupTitle("Update Profile Success");
-        setPopupButtonMessage("Close");
-        setPopupType(true);
-        alert(response.data.message);
-
-
-
+    if (response == undefined) {
+      setPopupTitle("Network Error!");
+      setPopupButtonMessage("Try Again");
+      setPopupType(false);
+      return;
     }
 
-    useEffect(() => {
-        checkToken();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        getUserProfile(setProfile);
+    if (response.status.toString()[0] != 2) {
+      // console.log(response)
+      setPopupTitle(response.data.message);
+      setPopupButtonMessage("Try Again");
+      setPopupType(false);
+      return;
+    }
 
-        getAllBankName(setBankName);
+    setPopupTitle("Update Profile Success");
+    setPopupButtonMessage("Close");
+    setPopupType(true);
+    alert(response.data.message);
+  }
 
-        return () => {
-            setProfile(null);
-        };
-    }, []);
+  useEffect(() => {
+    checkToken();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    getUserProfile(setProfile);
 
-    useEffect(() => {
-        if (profile != null) {
-            setBannerImageSrc([profile.header_picture]);
-            setProfileImageSrc([profile.profile_picture]);
-            setProfileMemberSince(new Date(profile.create_at));
-            setProfileName(profile.name);
-            setProfileEmail(profile.email);
-            setProfileDateOfBirth(
-                profile.date_of_birth != null && new Date(profile.date_of_birth)
-            );
-            setProfileHeadline(profile.headline);
-            setProfileBio(profile.bio);
-            setProfileCity(profile.city);
-            setProfileCountry(profile.country);
-            setProfileLastEducation(profile.last_education);
-            setProfileCurrentEducation(profile.current_education);
-            setProfileFieldOfStudy(profile.field_of_study);
-            setProfileYearofStudy(profile.year_of_study);
-            setProfileAccountNumber(profile.account_number);
-        }
-    }, [profile]);
+    getAllBankName(setBankName);
 
-    return {
-        isLogin,
-        user,
-        profile,
-        bankName,
-        profileAccountNumber,
-        setProfileAccountNumber,
-        saveProfile,
+    return () => {
+      setProfile(null);
     };
+  }, []);
+
+  useEffect(() => {
+    if (profile != null) {
+      setBannerImageSrc([profile.header_picture]);
+      setProfileImageSrc([profile.profile_picture]);
+      setProfileMemberSince(new Date(profile.create_at));
+      setProfileName(profile.name);
+      setProfileEmail(profile.email);
+      setProfileDateOfBirth(
+        profile.date_of_birth != null && new Date(profile.date_of_birth)
+      );
+      setProfileHeadline(profile.headline);
+      setProfileBio(profile.bio);
+      setProfileCity(profile.city);
+      setProfileCountry(profile.country);
+      setProfileLastEducation(profile.last_education);
+      setProfileCurrentEducation(profile.current_education);
+      setProfileFieldOfStudy(profile.field_of_study);
+      setProfileYearofStudy(profile.year_of_study);
+      setProfileAccountNumber(profile.account_number);
+    }
+  }, [profile]);
+
+  return {
+    isLogin,
+    user,
+    profile,
+    bankName,
+    profileAccountNumber,
+    setProfileAccountNumber,
+    saveProfile,
+  };
 }
