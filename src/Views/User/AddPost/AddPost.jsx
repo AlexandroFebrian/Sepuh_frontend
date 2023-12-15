7756709db7fac01efdfb545b14d563d2a18b9da7
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-12-15 09:07:13 +0700

    Update AddPost.jsx
    add back button

--- src/Views/User/AddPost/AddPost.jsx
+++ src/Views/User/AddPost/AddPost.jsx
@@ -5,10 +5,14 @@ import CompanyAddPost from './Company/CompanyAddPost';
 import { useSelector } from 'react-redux';
 import FreelancerDefaultMenu from '../../../components/SidebarMenu/Freelancer/FreelancerDefaultMenu/FreelancerDefaultMenu';
 import CompanyDefaultMenu from '../../../components/SidebarMenu/Company/CompanyDefaultMenu/CompanyDefaultMenu';
+import { FaArrowLeft } from 'react-icons/fa6';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddPost() {
   const user = useSelector((state) => state.user.userDetail)
   const isLogin = useSelector((state) => state.user.isLogin)
+
+  const navigate = useNavigate()
 
   return (
     <>
@@ -29,7 +33,13 @@ export default function AddPost() {
         </div>
         <div className="mid w-3/5">
           <div className=" min-h-[calc(100vh-5rem)] h-fit border-l-2 border-navyblue-600 z-0 px-10 py-10">
-            <h1 className='font-bold text-3xl'>New Post</h1>
+            <div className='flex items-center'>
+              <FaArrowLeft 
+                className=" w-9 h-9 mr-3 cursor-pointer"
+                onClick={() => navigate(-1)}
+              />
+              <h1 className='font-bold text-3xl'>New Post</h1>
+            </div>
             <div className='w-full bg-ghostwhite-100 mt-5 p-7 rounded shadow-lg'>
               {
                 user?.role == "Freelancer"
