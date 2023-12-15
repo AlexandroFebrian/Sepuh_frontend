a7dda033755bf9cf6360c8279e9a4ad8ddc70f31
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-12-15 09:48:00 +0700

    update landing page category

--- src/Views/User/LandingPage/Categories.jsx
+++ src/Views/User/LandingPage/Categories.jsx
@@ -5,54 +5,44 @@ export default function Categories() {
   const categories = [
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learningasdsadsa"
+      text: "Machine Learning",
+      value: "machine learning"
     },
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
+      text: "AI Development",
+      value: "ai development"
     },
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
+      text: "Blockchain & Cryptocurrency",
+      value: "blockchain & cryptocurrency"
     },
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
+      text: "Logo Design",
+      value: "logo design"
     },
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
+      text: "Art & Illustration",
+      value: "art & illustration"
     },
     {
       img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
-    },
-    {
-      img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
-    },
-    {
-      img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
-    },
-    {
-      img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
-    },
-    {
-      img_url: "/carousel/carous_1.jpg",
-      text: "Machine Learning"
-    },
+      text: "Midjourney Artists",
+      value: "midjourney artists"
+    }
     
   ]
 
   return (
     <>
       <div className='mb-16'>
-        <h1 className='text-4xl font-bold mb-5'>Categories</h1>
+        <h1 className='text-4xl font-bold mb-5'>Top Categories</h1>
         <div className='py-5 flex overflow-x-auto overflow-y-hidden h-[350px] custom-scrollbar'>
           {
-            categories.map((category, idx) => <CategoriesCard photo={category.img_url} key={idx}>{category.text}</CategoriesCard>)
+            categories.map((category, idx) => <CategoriesCard category={category} key={idx}></CategoriesCard>)
           }
           
         </div>
