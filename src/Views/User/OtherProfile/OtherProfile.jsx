02035de2059ca73a387ea1dbfdc3cb9dd77963b3
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-12-15 09:06:00 +0700

    Update OtherProfile.jsx
    add back button

--- src/Views/User/OtherProfile/OtherProfile.jsx
+++ src/Views/User/OtherProfile/OtherProfile.jsx
@@ -5,9 +5,12 @@ import OtherProfileViewModel from "./OtherProfileViewModel";
 
 import { useEffect, useState } from "react";
 import ProfileBox from "./ProfileBox";
+import { FaArrowLeft } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 export default function OtherProfile() {
   const { isLogin, user, profile, posts } = OtherProfileViewModel()
+  const navigate = useNavigate()
 
   return (
     <>
@@ -29,7 +32,13 @@ export default function OtherProfile() {
         </div>
         <div className="mid w-3/5 h-full">
           <div className=" min-h-[calc(100vh-5rem)] max-h-[calc(100vh-5rem)] border-l-2 border-navyblue-600 z-0 px-10 py-10">
-            <h1 className="font-bold text-3xl">View Profile {profile?.name}</h1>
+            <div className="flex items-center">
+              <FaArrowLeft 
+                className=" w-9 h-9 mr-3 cursor-pointer"
+                onClick={() => navigate(-1)}
+              />
+              <h1 className="font-bold text-3xl">View Profile {profile?.name}</h1>
+            </div>
             {
               profile
               &&
